a06f5bfc64585d6aaf5acdf8b88edf4bb5c5e667
Stan K <stan.kossovskiy@gmail.com>
2019-05-26 12:17:01 -0400

    Wire Open/Save commands

--- TestDrivenDev/TDD_PivotStructure/PivoteerWPF/src/MVVM/FileTreeViewModel.cs
+++ TestDrivenDev/TDD_PivotStructure/PivoteerWPF/src/MVVM/FileTreeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PivoteerWPF.Common;
+using PivoteerWPF.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +10,29 @@ namespace PivoteerWPF.MVVM
 {
     class FileTreeViewModel
     {
-
+        private Project _content;
         public FileTreeViewModel()
         {
             GalaSoft.MvvmLight.Messaging.Messenger.Default.Register<FileCommandMessage>(this, ReceiveFileCommand);
         }
 
-        private void ReceiveFileCommand(FileCommandMessage obj)
+        private void ReceiveFileCommand(FileCommandMessage fileCommand)
         {
-            throw new NotImplementedException();
+            switch(fileCommand.Command)
+            {
+                case "NEW":
+                    _content = new Project();
+                    ApplicationCommands.SaveJsonObject(fileCommand.Path, _content);
+                    break;
+                case "OPEN":
+                    _content = (Project) ApplicationCommands.ReadJsonObject(fileCommand.Path, typeof(Project));
+                    break;
+                case "SAVE":
+                    ApplicationCommands.SaveJsonObject(fileCommand.Path, _content);
+                    break;
+                default:
+                    throw new Exception($"Unknnown File Menu command {fileCommand.Command}");
+            }
         }
     }
 }
